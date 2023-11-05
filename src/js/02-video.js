173784d98fe32c1955a98a26688dd76e3a3ba54a
Shuster Oleksandr <godzilasanya@gmail.com>
2023-11-05 19:15:09 +0200

    -changed logic on setCurrentTime -task-3 Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,16 @@ import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
+// встановлення таймштампу з сховища при завантаженні вікна
+window.addEventListener('load', function () {
+  if (loadStorage('videoplayer-current-time')) {
+    player.setCurrentTime(loadStorage('videoplayer-current-time'));
+  }
+});
+//збереження таймштампу в сховище
 player.on(
   'timeupdate',
   throttle(function (currentTime) {
     saveStorage('videoplayer-current-time', currentTime.seconds);
   }, 1500)
 );
-
-if (loadStorage('videoplayer-current-time')) {
-  player.setCurrentTime(loadStorage('videoplayer-current-time'));
-}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import { debounce } from 'lodash';
+import { saveStorage, loadStorage } from './helpers/storage.js';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('.feedback-form input'),
+  inputMessage: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', debounce(handlerInput, 500));
+refs.form.addEventListener('submit', handlerSubmit);
+
+//заповенення форми при завантаженні вікна
+window.addEventListener('load', function () {
+  if (loadStorage('feedback-form-state')) {
+    const { email: sevedEmail, message: savedMessage } = loadStorage(
+      'feedback-form-state'
+    );
+    refs.inputEmail.value = sevedEmail;
+    refs.inputMessage.value = savedMessage;
+  }
+});
+
+// збереження даних з форми в сховище
+const userData = { email: '', message: '' };
+function handlerInput(event) {
+  if (event.target.name === 'email') {
+    userData.email = event.target.value;
+  }
+  if (event.target.name === 'message') {
+    userData.message = event.target.value;
+  }
+  saveStorage('feedback-form-state', userData);
+}
+
+// виведення даних з форми в консоль, очистка сховища та форми
+function handlerSubmit(event) {
+  const email = refs.inputEmail.value;
+  const message = refs.inputMessage.value;
+  event.preventDefault();
+  if (email === '' || message === '') {
+    alert("Відсутні обов'язкові поля");
+  } else {
+    console.log({ email, message });
+    refs.form.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+}
