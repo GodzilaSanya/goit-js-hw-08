ccf1f9537b3b14508616aaacc3346cde85feb7b6
Shuster Oleksandr <godzilasanya@gmail.com>
2023-11-05 17:02:16 +0200

    task-2 Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { saveStorage, loadStorage } from './helpers/storage';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    saveStorage('videoplayer-current-time', currentTime.seconds);
+  }, 1500)
+);
+
+if (loadStorage('videoplayer-current-time')) {
+  player.setCurrentTime(loadStorage('videoplayer-current-time'));
+}
